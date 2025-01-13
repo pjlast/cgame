d17fced0f64dd0d9a66ae2268063a8acd7b31f83
PJ Last <petri.last@gmail.com>
2025-01-13 20:14:34 +0200

    basic menu options

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include <stdbool.h>
+#include <stdio.h>
 
 typedef enum { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
 
@@ -21,11 +22,53 @@ bool draw_logo() {
   return false;
 }
 
+void drawMenu(int xOffset, int yOffset, int numOptions, char **options,
+              int selectedMenuOption) {
+  for (int i = 0; i < numOptions; i++) {
+    if (i == selectedMenuOption) {
+      char buf[256];
+      snprintf(buf, sizeof(buf), "> %s", options[selectedMenuOption]);
+      DrawText(buf, xOffset, yOffset + (16 * i), 16, WHITE);
+    } else {
+      DrawText(options[i], xOffset, yOffset + (16 * i), 16, WHITE);
+    }
+  }
+}
+
 void draw_title() {
+  static int selectedMenuOption = 0;
+
+  if (IsKeyPressed(KEY_UP)) {
+    selectedMenuOption--;
+    if (selectedMenuOption < 0) {
+      selectedMenuOption = 1;
+    }
+  }
+
+  if (IsKeyPressed(KEY_DOWN)) {
+    selectedMenuOption++;
+    if (selectedMenuOption > 1) {
+      selectedMenuOption = 0;
+    }
+  }
+
+  if (IsKeyPressed(KEY_ENTER)) {
+    if (selectedMenuOption == 1) {
+      CloseWindow();
+    }
+  }
+
   BeginDrawing();
 
   ClearBackground(BLUE);
   DrawText("TITLE SCREEN", 120, 120, 30, BLACK);
+
+  char *options[] = {
+      "Start Game",
+      "Quit",
+  };
+
+  drawMenu(120, 140, 2, options, selectedMenuOption);
 
   EndDrawing();
 }
